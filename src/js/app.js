67a8de49657ca68c85e0f5d2dd93e64fbfe17722
vildsee <78222196+vildsee@users.noreply.github.com>
2022-11-13 21:09:32 +0100

    img change

---
+++ src/js/app.js
@@ -0,0 +1,44 @@
+const reviews = [
+  {
+    id: 1,
+    name: "",
+    job: "",
+    img: "",
+    text: "",
+  },
+  {
+    id: 2,
+    name: "",
+    job: "",
+    img: "",
+    text: "",
+  },
+  {
+    id: 3,
+    name: "",
+    job: "",
+    img: "",
+    text: "",
+  },
+  {
+    id: 4,
+    name: "",
+    job: "",
+    img: "",
+    text: "",
+  },
+  {
+    id: 5,
+    name: "",
+    job: "",
+    img: "",
+    text: "",
+  },
+  {
+    id: 6,
+    name: "",
+    job: "",
+    img: "",
+    text: "",
+  },
+];
